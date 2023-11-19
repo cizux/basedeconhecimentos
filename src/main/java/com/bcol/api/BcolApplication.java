8b6135525ae7664baf59e244fd2dda24b3cc4216
Tarcisio Paz Junger <tarcisiojunger@gmail.com>
2023-11-18 22:41:07 -0300

    Project created

---
+++ src/main/java/com/bcol/api/BcolApplication.java
@@ -0,0 +1,13 @@
+package com.bcol.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bcol/api/BcolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcol.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
